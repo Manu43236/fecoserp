3c25f253f6ccbc6e3fdf3de06fffe79bc755189d
Manikanta Chadaram <manikanta.chadaram1992@gmail.com>
2026-08-21 12:00:26 +0530

    issue fixed

--- fecos-api/src/main/resources/db/migration/V45__fix_pump_tables_base_columns.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE pumps
-    ADD COLUMN updated_at  TIMESTAMP NULL,
-    ADD COLUMN created_by  CHAR(36)  NULL;
-
-ALTER TABLE pump_maintenance_logs
-    ADD COLUMN updated_at  TIMESTAMP NULL,
-    ADD COLUMN created_by  CHAR(36)  NULL;
